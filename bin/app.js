5706f9f85e9ec227106d339a2fc99f7abdb12cfa
Rottencop <rottencop@mailinator.com>
2017-07-03 16:54:38 +0530

    WIP

--- bin/app.js
+++ bin/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const loader = require('require-dir');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const chalk = require('chalk');
 
 mongoose.connect('mongodb://'+process.env.db_host+'/'+process.env.db_name);
 mongoose.Promise = global.Promise;
@@ -13,11 +14,15 @@ var app = express();
 
 app.use(morgan('combined'));
 
-
+var routes = loader('../routes');
+Object.keys(routes).forEach((route)=>{
+    app.use("/"+route,routes[route]);
+    console.log(chalk.yellow("Loading route : "+route));
+});
 
 var api_port = process.env.api_port;
 api_port = parseInt(api_port,10);
 app.listen(api_port,()=>{
-    console.log("Http server running on port : "+api_port);
+    console.log(chalk.green.bold("Http server running on port : "+api_port));
 });
 module.export = app;
