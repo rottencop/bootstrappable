a08878a9d0f85d9c38588d12faa4bf1ba258bcc9
Rottencop <rottencop@mailinator.com>
2017-07-03 17:16:19 +0530

    WIP

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const auth_lib = require('../lib/auth');
 
 var route = express.Router();
 
-route.get("/",(req,res)=>{
-    
+// Route to create new user
+route.post("/",(req,res)=>{
+    new auth_lib(req.body)
+        .signup()
+        .then((user)=>{
+            res.status(201).end();
+        })
+        .catch((err)=>{
+            res.status(400).json({"err": err.message});
+        });
 });
+
+
+// Route to generate new auth token
+route.post("/token",(req,res)=>{
+    new auth_lib(req.body)
+        .token()
+        .then((token)=>{
+            res.status(200).json(token);
+        })
+        .catch((err)=>{
+            res.status(400).json({"err": err.message});
+        });
+});
+
 
 module.exports = route;
